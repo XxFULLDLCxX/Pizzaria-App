c2577369ff6d103d35d50fd0dcc8b57199bf6023
José Matheus <XxFULLDLCxX@outlook.com>
2024-03-30 00:03:17 -0300

    temporary ...

---
+++ src/middleware.tsx
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export const config = {
+  matcher: '/((?!_next/static|_next/image|favicon.ico).*)',
+};
+
+const publicRoutes = ['/', '/about-me', '/auth/sign-up', '/auth/sign-in'];
+
+// export async function middleware(req: NextRequest) {
+//   console.log(req.nextUrl);
+
+//   const pathname = req.nextUrl.pathname;
+
+//   if (publicRoutes.includes(pathname)) {
+//     return NextResponse.next();
+//   }
+
+//   // const session = await AuthService.isSessionValid();
+//   // if (!session) {
+//   //   const isAPIRoute = pathname.startsWith('/api');
+
+//     // if (isAPIRoute) {
+//     //   return NextResponse.json({ message: 'Não autorizado' }, { status: 401 });
+//     // }
+
+//   //   return NextResponse.redirect(new URL('/portal/login', req.url));
+//   // }
+
+//   return NextResponse.next();
+// }
+
+const logger = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.info('dispatching', action);
+  let result = next(action);
+  console.log('next state', store.getState());
+  console.groupEnd();
+  return result;
+};
+
+export default logger;
